0795981c935ca4681a9f137725bc06fa5222fcaf
Gabriele Armento <gabriele.armento@gmail.com>
2022-02-18 15:45:48 +0100

    refactored core.py

--- hermes/core.py
+++ hermes/core.py
@@ -1,126 +1,124 @@
-import re
 from aliases import aliases
+from typing import Union, Callable
+import re, logging
+
+logging.basicConfig()
+log = logging.getLogger('hermes')
+log.setLevel(logging.DEBUG)
+
+
+class TooManFeatures(ValueError):
+    '''Raises when there are too many features of a certain kind in the same signal.'''
+
+    def __init__(self, message, text):
+        self.message = message
+        self.text = text
+        super().__init__(message, text)
+
+
+class Hermes:
+
+    def __init__(self, text: str):
+        self.text = self.preprocess(text)
+        self.features = {}
+
+    # TODO
+    def preprocess(self, text: str) -> str:
+        return text
+
+    def search_algo(self, algo: Callable, *args, allow_this_many=1) -> list:
+        result_algo = algo(self.text, *args)
+        log.debug(f'doing {algo.__name__}, found {result_algo}')
+        if isinstance(result_algo, list):
+            if len(result_algo) > allow_this_many:
+                raise TooManyFeatures(f'{algo.__name__} = {result_algo}',
+                                      self.text)
+            return result_algo
+        return list(result_algo)
+
+
+def search_regex(text: str, regex: str) -> Union[None, list]:
+    # i'm not sure this is actually effective
+    compiled = re.compile(regex, re.I | re.M)
+    search_result = compiled.findall(text)
+    return search_result
+
+
+def search_symbol(text: str):
+    '''Sequentially goes through all of the aliases until it finds a match'''
+    results = list()
+    for k in aliases:
+
+        # unpacking the dict
+        comp = '|'.join([k, *aliases[k]])
+        # i.e. '\b(XAUUSD|gold|xau)\b'
+        regex = rf"(?:\b({comp})\b)"
+
+        search_result = search_regex(text, regex)
+        if search_result:
+            log.debug(f'found {k}')
+            results.append(k)
+
+    return list(set(results))
+
+
+def search_close(text: str):
+    # manual check close
+    blacklist = [
+        'if', 'candle', 'closer', 'will', 'not', 'might', 'minute', 'daily',
+        'support', 'been'
+    ]
+    if 'clos' in text.lower():
+        if all(x not in text.lower() for x in blacklist):
+            if len(text) <= 100:
+                return [text]
+    return list()
+
 
 def interpret(text: str) -> dict:
 
-    log = logging.getLogger('intepreter')
-    
-    # we'll use a std dict to return the values
-    cols = "symbol, sl, entry, side, tps, flag".split(', ')
-    data = {k: None for k in cols}
-    
-    # goes through all the aliases to find a match in the text
-    for k in aliases:
+    SL = r'(?:sl|stops?|stop ?loss)(?:\W{1,3})(\d{1,6}[\.\:]{0,2}\d{0,5})'
+    ENTRY = r'(?:entry|price)(?:\W{1,3})(\d{1,6}[\.\:]{0,2}\d{0,5})'
+    TP = r'(?:tp|take profits?)(?: ?\d?[^a-zA-Z0-9.]{1,3})(\d{1,6}[\.\:]{0,2}\d{1,5})'
+    SIDE = r'(?:\b(buys?!?|longs?!?|sells?!?|shorts?!?)\b)'
+    PARTIALS = r'(?:\b((?:take )?partials?|take profits?)\b)'
+    BREAKEVEN = r'(?:\b((?:stops? (?:loss )?|sl )to (?:break ?even|bep?|entry))\b)'
+    MOVE_SL = r"(?:\b(?:stops? (?:loss )?(?:to )?(\d{1,6}[\.\:]{0,2}\d{0,5}))\b)"
 
-        comp = '|'.join([k, *aliases[k]])         # unpacking the dict
-        reg = rf"(?:\b({comp})\b)"                # i.e. '\b(XAUUSD|gold|xau)\b'
-        pair = re.compile(reg, re.I|re.M)
-        
-        if pair.search(text):
-            data['symbol'] = k
-            break
+    hermes = Hermes(text)
 
-    # compiling sl regex
-    slRE = r'(?:sl|stops?|stop ?loss)(?:\W{1,3})(\d{1,6}[\.\:]{0,2}\d{0,5})'
-    slC = re.compile(slRE, re.I|re.M)
+    data = {
+        'symbol': hermes.search_algo(search_symbol),
+        'entry': hermes.search_algo(search_regex, ENTRY),
+        'sl': hermes.search_algo(search_regex, SL),
+        'tp': hermes.search_algo(search_regex, TP, allow_this_many=10),
+        'side': hermes.search_algo(search_regex, SIDE),
+        'partials': hermes.search_algo(search_regex, PARTIALS),
+        'breakeven': hermes.search_algo(search_regex, BREAKEVEN),
+        'move_sl': hermes.search_algo(search_regex, MOVE_SL),
+        'close': hermes.search_algo(search_close)
+    }
 
-    # compiling entry regex
-    entryRE = r'(?:entry|price)(?:\W{1,3})(\d{1,6}[\.\:]{0,2}\d{0,5})'
-    entryC = re.compile(entryRE, re.I|re.M)
+    if data['symbol'] and data['sl'] and data['side']:
+        data['flag'] = 'POSITION'
 
-    # compiling tp regex
-    tpRE = r'(?:tp|take profits?)(?: ?\d?[^a-zA-Z0-9.]{1,3})(\d{1,6}[\.\:]{0,2}\d{1,5})'
-    tpC = re.compile(tpRE, re.I|re.M)
+    else:
+        allowed = 'partials breakeven move_sl close'.split(' ')
+        try:
+            data['flag'] = [x for x in data if (data[x] and x in allowed)
+                            ][0].upper()
+        except IndexError:
+            data['flag'] = None
 
-    # compiling side regex
-    sideRE = r'(?:\b(buys?!?|longs?!?|sells?!?|shorts?!?)\b)'
-    sideC = re.compile(sideRE, re.I|re.M)
-
-    # compiling partials regex
-    partialsRE = r'(?:\b((?:take )?partials?|take profits?)\b)'
-    partialsC = re.compile(partialsRE, re.I|re.M)
-
-    # compiling breakeven regex
-    beRE = r'(?:\b((?:stops? (?:loss )?|sl )to (?:break ?even|bep?|entry))\b)'
-    beC = re.compile(beRE, re.I|re.M)
-    
-    # move_sl
-    move_slRE = r"(?:\b(?:stops? (?:loss )?(?:to )?(\d{1,6}[\.\:]{0,2}\d{0,5}))\b)"
-    move_slC = re.compile(move_slRE, re.I|re.M)
-    
-    # manual check close
-    close_match = None
-    blacklist = ['if', 'candle', 'closer', 'will', 'not', 'might', 'minute', 'daily', 'support', 'been']
-    if 'clos' in text.lower():
-        if all(x not in text.lower() for x in blacklist):
-            if len(text)<=100:
-                close_match = text
-
-    # regex results
-    sl_match = slC.search(text)
-    entry_match = entryC.search(text)
-    side_match = sideC.search(text)
-    partials_match = partialsC.search(text)
-    tp_match = tpC.findall(text)
-    be_match = beC.search(text)
-    move_sl_match = move_slC.search(text)
-
-    try: # assigns values
-        data['sl'] = sl_match.groups()[0]
-    except AttributeError:
-        pass
-    
-    try:
-        data['entry'] = entry_match.groups()[0]
-    except AttributeError:
-        pass
-    
-    try:
-        data['side'] = SIDE.BUY if any(
-            w in side_match.groups()[0].lower() for w in ['buy', 'long']) else SIDE.SELL
-    except AttributeError:
-        try: # there is no side data but let's see if we can infer it from the numbers
-            if data['entry'] > data['sl']:
-                data['side'] = SIDE.BUY
-            else: data['side'] = SIDE.SELL
-        except TypeError:
-            pass
-        
-    # adds tps
-    data['tps'] = tp_match
-
-    # if data[side] is missing it means that there was no sl, no entry and no direction
-    if data['side'] is None:
-
-        # could be a close signal
-        if close_match is not None:
-            data['flag'] = FLAG.CLOSE
-
-        elif be_match is not None:
-            data['flag'] = FLAG.BREAKEVEN
-        
-        elif move_sl_match is not None:
-            data['flag'] = FLAG.MOVE_SL
-            data['sl'] = move_sl_match.groups()[0]
-
-        # could be a partials signal
-        elif partials_match is not None:
-            data['flag'] = FLAG.PARTIALS
-
-        # could be a update-tp
-        elif len(tp_match) > 0:
-            data['flag'] = FLAG.UPDATE_TP
-
-        else: # it's just a text
-            data['flag'] = FLAG.MESSAGE
-
-        return data
-
-    # no sl, no symbol or no side, returns with 'no-data' flag
-    if any(x is None for x in [sl_match, data['symbol'], data['side']]):
-        data['flag'] = FLAG.ERROR
-        return data
-
-    # the only other case is limit & market order
-    data['flag'] = FLAG.ORDER
     return data
+
+
+def main():
+
+    test = 'Entry: 1.15, SL: 1.12, Side: longs!!!, SL:11.5 Pair: EURUSD!'
+    log.debug(test)
+    print(interpret(test))
+
+
+if __name__ == '__main__':
+    main()
