452b266bd382a12f2cf8f106c2274bfacb79b1ad
Gabriele Armento <gabriele.armento@gmail.com>
2022-02-18 12:08:38 +0100

    restructured project

---
+++ setup.py
@@ -0,0 +1,20 @@
+afrom setuptools import setup, find_packages
+import hermes
+
+with open('./README.md' , 'r', encoding = 'utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='hermes',
+    version=mt5.__version__,
+    author=mt5.__author__,
+    author_email='contact@gabrielearmento.com',
+    description='A signal parser to understand human trading signals',
+    long_description_content_type='text/markdown',
+    long_description=readme,
+    packages=find_packages(where='hermes'),
+    url='https://github.com/965311532/hermes',
+    license=None,
+    install_requires=['spacy'],
+    python_requires='>=3.6'
+    )
